53c456132734fe26ee8bc09cf040575352506be2
ganeshmoorthy <ganeshmoorthy@gmail.com>
2015-02-27 18:35:24 +0530

    semi colon added

--- src/com/projecteuler/net/LargeSum.java
+++ src/com/projecteuler/net/LargeSum.java
@@ -110,8 +110,11 @@ public class LargeSum
         
         for (int i = 0; i < data.length();)
         {
+        	
             i++;
-            temp = temp + Double.parseDouble(data.substring(i - 1, i));//
+            
+            temp = temp + Double.parseDouble(data.substring(i - 1, i));;
+            
         }
         
         System.out.println(temp);
